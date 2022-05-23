d211d7ecbc0f6b83068f75f848cd8c46eb7bbfee
lkirazc1 <97258501+lkirazc1@users.noreply.github.com>
2022-05-23 13:21:31 -0700

    Add files via upload

---
+++ utils/settings.py
@@ -0,0 +1,31 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+FPS = 200
+
+WIDTH, HEIGHT = 600, 700
+
+ROWS = COLS = 50
+
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+PIXEL_SIZE = WIDTH // COLS
+
+BG_COLOR = WHITE
+
+DRAW_GRID_LINES = False
+
+def get_font(size):
+    return pygame.font.SysFont("comicsans", size)
+
+
+
+
